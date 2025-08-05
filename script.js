8fe45b6839d233c91878acae8fbe82cd65adb4ac
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-05 15:58:32 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const Score = 20;
+
+if (Score >= 18) {
+  console.log("Nice");
+}else if (Score >= 10 && Score < 18) {
+  console.log("To Ghabol Shody");
+}else {
+  console.log("To Rad Shody");
+}
